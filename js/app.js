a461f20cc96e2e8d51e6babdbefeb273b6f24588
Sardar Anonymous <zabisardar65@gmail.com>
2026-09-11 22:02:49 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-console.log("Campus Cleanliness Heat Map loaded successfully.");
+// Temporary dashboard data
+// Later these values will come from the database.
+
+const dashboardData = {
+    totalComplaints: 36,
+    highAreas: 1,
+    resolvedComplaints: 14,
+    weeklyComplaints: 12
+};
+
+
+// Display dashboard statistics
+
+document.getElementById("totalComplaints").textContent =
+    dashboardData.totalComplaints;
+
+document.getElementById("highAreas").textContent =
+    dashboardData.highAreas;
+
+document.getElementById("resolvedComplaints").textContent =
+    dashboardData.resolvedComplaints;
+
+document.getElementById("weeklyComplaints").textContent =
+    dashboardData.weeklyComplaints;
